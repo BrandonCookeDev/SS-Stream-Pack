d9263fe0fe7578bbc269b3c0ac6ffe2343365aee
Brandon Cooke <brandoncookedev@gmail.com>
2018-07-15 14:43:46 -0400

    Added ports and teams. Heavily modified vue.js code to begin working on rotating images and player tag-twitter names

--- resources/js/vue-main.minimal.js
+++ resources/js/vue-main.minimal.js
@@ -1,15 +1,5 @@
-
-function join(){
-  let s = '';
-  for(var i in arguments){
-    var arg = arguments[i];
-    s += arg 
-    
-    if(i != arguments.length - 1) 
-      s += '/'
-  };
-  return s;
-}
+import '../../node_modules/moment';
+import '../../node_modules/axios';
 
 /** FILE SYSTEMS */
 const RESOURCES_DIR = '../resources'
@@ -18,21 +8,31 @@ const FONTS_DIR     = join(RESOURCES_DIR, 'fonts');
 const STYLES_DIR    = join(RESOURCES_DIR, 'styles');
 const VIDEOS_DIR    = join(RESOURCES_DIR, 'videos');
 
-//These paths are relative to the Overlay file
-//const CHARACTER_DIR = '../../Overlays/Characters/'
+//Images
 const CHARACTER_DIR   = join(IMAGES_DIR, 'Characters');
 const PORT_DIR        = join(IMAGES_DIR, 'Ports');
 const OVERLAY_DIR     = join(IMAGES_DIR, 'Overlays');
+const SPONSOR_DIR     = join(IMAGES_DIR, 'Sponsors');
 const FLAG_DIR        = join(IMAGES_DIR, 'Flags/64flat');
+const EYES_DIR        = join(IMAGES_DIR, 'PlayerEyes');
+
+//Videos
+const WEBM_DIR        = join(VIDEOS_DIR, 'webm');
+const MELEE_WEBM      = join(WEBM_DIR, 'Melee');
+const SMASH4_WEBM     = join(WEBM_DIR, 'Smash4');
 const MELEE_CHAR_DIR  = join(CHARACTER_DIR, 'Melee');
 const SMASH4_CHAR_DIR = join(CHARACTER_DIR, 'Smash 4');
-const EYES_DIR        = '../../CIlvanis Masks/Crops/'
 
-var POLL_INTERVAL = 500;
-var ROUND_INTERVAL = 10000;
-var JSON_PATH = JSON_PATH || '../StreamControl_0_4b/streamcontrol.json';
+//JSON Polling and Intervals
+var LEGAL_IMAGE_STATES = ['FLAG', 'SPONSOR'];
+var LEGAL_NAMEPLATE_STATES = ['TAG', 'TWITTER'];
+const POLL_INTERVAL = 500;
+const IMAGE_STATE_INTERVAL = 5000;
+const NAMEPLATE_STATE_INTERVAL = 5000;
+const JSON_PATH = JSON_PATH || '../StreamControl_0_4b/streamcontrol.json';
 
 var port         = 11769;
+const ROUND_INTERVAL = 10000;
 var smashGGinit  = 'http://localhost:'+port+'/init/';
 var smashGGround = 'http://localhost:'+port+'/getMatch';
 
@@ -77,6 +77,18 @@ function getFlag(name){
   return join(FLAG_DIR, name + '.png');
 }
 
+function getSponsor(sponsor){
+  return join(SPONSOR_DIR, sponsor + '.png');
+}
+
+function getMeleeMUCharacter(character){
+  return join(MELEE_WEBM, 'Characters', character + '.webm');
+}
+
+function getS4MUCharacter(character){
+  return join(SMASH4_WEBM, 'Characters', character + '.webm');
+}
+
 /**
  * Vue Application
  */
@@ -85,6 +97,11 @@ var app = new Vue({
   data: {
     /* INFO OBJECT LINKS TO THE JSON CREATED BY STREAM CONTROL */
     info: {
+        IMAGE_ROTATION_ON: false,
+        NAMEPLATE_ROTATION_ON: false,
+        IMAGE_STATE: 'FLAG',
+        NAMEPLATE_STATE: 'TAG',
+
         pull_mode: 'PULL_ALL',
         event_countdown: 0,
         event_notice: '',
@@ -112,105 +129,117 @@ var app = new Vue({
     timestamp: new Date()
   },
   watch: {
+    "info.IMAGE_STATE":{
+
+    },
+    "info.NAMEPLATE_STATE":{
+
+    },
+    "info.timer": {
+
+    }
   },
   computed: {
-    game_header: function(){
+    gameHeader: function(){
       return this.info.event_round + ' - ' + this.info.best_of_x;
     },
-    char_1_img: function(){
+    player1FlagSponsor: function(){
+      
+    },
+    player1Character: function(){
       return getMeleeChar(this.info.p1_char);
     },
-    char_2_img: function(){
+    player2Character: function(){
       return getMeleeChar(this.info.p2_char);
     },
-    char_3_img: function(){
+    player3Character: function(){
       return getMeleeChar(this.info.p3_char);
     },
-    char_4_img: function(){
+    player4Character: function(){
       return getMeleeChar(this.info.p4_char);
     },
-    char_1_img_s4: function(){
+    player1CharacterS4: function(){
       return getSmash4Char(this.info.p1_char_s4);
     },
-    char_2_img_s4: function(){
+    player2CharacterS4: function(){
       return getSmash4Char(this.info.p2_char_s4);
     },
-    char_3_img_s4: function(){
+    player3CharacterS4: function(){
       return getSmash4Char(this.info.p3_char_s4);
     },
-    char_4_img_s4: function(){
+    player4CharacterS4: function(){
       return getSmash4Char(this.info.p4_char_s4);
     },
-    p1_port_img: function(){
+    player1PortImg: function(){
       return getPort(this.info.p1_port_color);
     },
-    p2_port_img: function(){
+    player2PortImg: function(){
       return getPort(this.info.p2_port_color);
     },
-    p3_port_img: function(){
+    player3PortImg: function(){
       return getPort(this.info.p3_port_color);
     },
-    p4_port_img: function(){
+    player4PortImg: function(){
       return getPort(this.info.p4_port_color);
     },
-    p1_port_img_s4: function(){
+    player1PortImgS4: function(){
       return getPort(this.info.p1_port_color_s4);
     },
-    p2_port_img_s4: function(){
+    player2PortImgS4: function(){
       return getPort(this.info.p2_port_color_s4);
     },
-    p3_port_img_s4: function(){
+    player3PortImgS4: function(){
       return getPort(this.info.p3_port_color_s4);
     },
-    p4_port_img_s4: function(){
+    player4PortImgS4: function(){
       return getPort(this.info.p4_port_color_s4);
     },
-    p1_flag_img: function(){
+    player1FlagImg: function(){
       return getFlag(this.info.p1_country);
     },
-    p2_flag_img: function(){
+    player2FlagImg: function(){
       return getFlag(this.info.p2_country);
     },
-    p3_flag_img: function(){
+    player3FlagImg: function(){
       return getFlag(this.info.p3_country);
     },
-    p4_flag_img: function(){
+    player4FlagImg: function(){
       return getFlag(this.info.p4_country);
     },
-    p1_flag_img_s1: function(){
+    player1FlagImgS4: function(){
       return getFlag(this.info.p1_country_p4);
     },
-    p2_flag_img_s2: function(){
+    player2FlagImgS4: function(){
       return getFlag(this.info.p2_country_p4);
     },
-    p3_flag_img_s3: function(){
+    player3FlagImgS4: function(){
       return getFlag(this.info.p3_country_p4);
     },
-    p4_flag_img_s4: function(){
+    player4FlagImgS4: function(){
       return getFlag(this.info.p4_country_p4);
     },
-    p1_eyes_img_s4: function(){
+    player1Eyes: function(){
       return getEyes(this.info.p1_eyes_s4);
     },
-    p2_eyes_img_s4: function(){
+    player2Eyes: function(){
       return getEyes(this.info.p2_eyes_s4);
     },
-    p3_eyes_img_s4: function(){
+    player3Eyes: function(){
       return getEyes(this.info.p3_eyes_s4);
     },
-    p4_eyes_img_s4: function(){
+    player4Eyes: function(){
       return getEyes(this.info.p4_eyes_s4);
     },
-    p1_eyes_img_s42: function(){
+    player1EyesS4: function(){
       return (getEyes(this.info.p1_name_s4.replace(/[\s]*<T>[\s\S]*<\/T>[\s]*/, '')).trim());
     },
-    p2_eyes_img_s42: function(){
+    player2EyesS4: function(){
       return (getEyes(this.info.p2_name_s4.replace(/[\s]*<T>[\s\S]*<\/T>[\s]*/, '')).trim());
     },
-    p3_eyes_img_s42: function(){
+    player3EyesS4: function(){
       return (getEyes(this.info.p3_name_s4.replace(/[\s]*<T>[\s\S]*<\/T>[\s]*/, '')).trim());
     },
-    p4_eyes_img_s42: function(){
+    player4EyesS4: function(){
       return (getEyes(this.info.p4_name_s4.replace(/[\s]*<T>[\s\S]*<\/T>[\s]*/, '')).trim());
     },
     formattedDate: function() {
@@ -230,6 +259,14 @@ var app = new Vue({
       axios.get(JSON_PATH, { responseType: 'json' })
         .then(resp => { this.info = resp.data; })
         .catch(resp => { console.error(resp); });
+    },
+    changeImageState: function(){
+      LEGAL_IMAGE_STATES = cycleArray(LEGAL_STATES);
+      this.info.IMAGE_STATE = LEGAL_IMAGE_STATES[0];
+    },
+    changeNameplayState: function(){
+      LEGAL_NAMEPLATE_STATES = cycleArray(LEGAL_NAMEPLATE_STATES);
+      this.into.NAMEPLATE_STATE = LEGAL_NAMEPLATE_STATES[0];
     }
   },
   // Triggered when the vue instance is created, triggers the initial setup.
@@ -237,9 +274,8 @@ var app = new Vue({
     this.loadJSON();
     setInterval(() => { this.timestamp = new Date(); }, 1000);
     setInterval(this.loadJSON, POLL_INTERVAL);
-  },
-  portClass: function(left_right, color){
-    return color + '-' + left_right;
+    setInterval(this.changeImageState, IMAGE_STATE_INTERVAL);
+    setInterval(this.changeNameplateState, NAMEPLATE_STATE_INTERVAL);
   }
 });
 
@@ -251,6 +287,41 @@ var app = new Vue({
  */
 function zeroPad(number) {
   return number < 10 ? '0' + number : '' + number;
+}
+
+/**
+ * Cycle the first element of an array to the back
+ * @param {Array} arr 
+ */
+function cycleArray(arr){
+  let first = arr.shift();
+  arr.push(first);
+  return arr;
+}
+
+function fileExists(filepath){
+  var http = new XMLHttpRequest();
+
+  http.open('HEAD', filepath, false);
+  http.send();
+
+  return http.status != 404;
+}
+
+/**
+ * Join a variable amount of arguments with '/' as a 
+ * seperator
+ */
+function join(){
+  let s = '';
+  for(var i in arguments){
+    var arg = arguments[i];
+    s += arg 
+    
+    if(i != arguments.length - 1) 
+      s += '/'
+  };
+  return s;
 }
 
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
